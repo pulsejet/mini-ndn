70706c6f48b82bb22eace4cb4c6a180169a0477b
Varun Patil <varunpatil@ucla.edu>
2023-03-22 04:01:12 +0000

    play: refactor PTY

--- minindn/play/term/pty.py
+++ minindn/play/term/pty.py
@@ -21,59 +21,67 @@ class PtyManager:
     poller = select.poll()
 
     def __init__(self, executor):
+        """Initialize the pty manager"""
         self.executor = executor
-        self.thread = Thread(target=self.ui_out_pty_thread, args=(), daemon=True)
+        self.thread = Thread(target=self._ui_thread, args=(), daemon=True)
         self.thread.start()
 
     def register(self, pty: 'Pty'):
+        """Register a new pty with the manager"""
         self.ptys[pty.master] = pty
         self.poller.register(pty.master, select.POLLIN)
 
     def unregister(self, pty: 'Pty'):
+        """Unregister a pty from the manager"""
         self.poller.unregister(pty.master)
         pty.cleanup()
         if pty.master in self.ptys:
             del self.ptys[pty.master]
 
-    # Send output to UI thread
-    def ui_out_pty_thread(self):
+    def _ui_thread(self):
+        """Thread that handles UI output"""
         while True:
-            # Check all processes
-            for pty in list(self.ptys.values()):
-                if pty.process is not None and pty.process.poll() is not None:
-                    self.unregister(pty)
-                    continue
+            self._check_procs()
+            self._poll_fds()
 
-            # Check all file descriptors and block for 1/4 s
-            for (fd, status) in self.poller.poll(250):
-                if fd not in self.ptys:
-                    self.poller.unregister(fd)
-                    continue
-                pty = self.ptys[fd]
+    def _check_procs(self):
+        """Check if any processes have exited and unregister them"""
+        for pty in list(self.ptys.values()):
+            if pty.process is not None and pty.process.poll() is not None:
+                self.unregister(pty)
+                continue
 
-                # Check if poller is closed
-                if status == select.POLLHUP:
-                    self.unregister(pty)
-                    continue
+    def _poll_fds(self):
+        """Poll all registered file descriptors"""
+        for (fd, status) in self.poller.poll(250):
+            if fd not in self.ptys:
+                self.poller.unregister(fd)
+                continue
+            pty = self.ptys[fd]
 
-                # Find the number of bytes available to read
-                bytes_available = fcntl.ioctl(pty.master, termios.FIONREAD, struct.pack('I', 0))
-                bytes_available = struct.unpack('I', bytes_available)[0]
-                bytes_to_read = min(bytes_available, 4096)
+            # Check if poller is closed
+            if status == select.POLLHUP:
+                self.unregister(pty)
+                continue
 
-                # This should never really happen
-                if bytes_to_read == 0:
-                    continue
+            # Find the number of bytes available to read
+            bytes_available = fcntl.ioctl(pty.master, termios.FIONREAD, struct.pack('I', 0))
+            bytes_available = struct.unpack('I', bytes_available)[0]
+            bytes_to_read = min(bytes_available, 4096)
 
-                # Read everything available and send to UI
-                try:
-                    bytes = os.read(pty.master, bytes_to_read)
-                    self.executor._send_pty_out(bytes, pty.id)
-                    pty.buffer.write(bytes)
-                except Exception as e:
-                    print(e)
-                    self.unregister(pty)
-                    continue
+            # This should never really happen
+            if bytes_to_read == 0:
+                continue
+
+            # Read everything available and send to UI
+            try:
+                bytes = os.read(pty.master, bytes_to_read)
+                self.executor._send_pty_out(bytes, pty.id)
+                pty.buffer.write(bytes)
+            except Exception as e:
+                print(e)
+                self.unregister(pty)
+                continue
 
 class Pty:
     id: str
@@ -86,6 +94,10 @@ class Pty:
     process: Optional[subprocess.Popen] = None
 
     def __init__(self, executor, id: str, name: str):
+        """
+        Initialize a new pty
+        Creates a new Python PTY instance and stores the slave and master file descriptors
+        """
         self.master, self.slave = pty.openpty()
         self.buffer = CircularByteBuffer(16000)
         self.executor = executor
@@ -95,6 +107,8 @@ class Pty:
         executor.pty_list[id] = self
 
     def cleanup(self):
+        """Cleanup the pty"""
+
         self.executor.socket.send_all(msgpack.dumps({
             WSKeys.MSG_KEY_FUN: WSFunctions.CLOSE_TERMINAL,
             WSKeys.MSG_KEY_ID: self.id,
